33eb9cda200de40d4514b7ce535fff3fce3d1fd3
Sebastian Meier <contact@sebastianmeier.eu>
2018-05-07 17:23:58 +0200

    ie and edge get raster tiles

--- app/js/main.js
+++ app/js/main.js
@@ -77,27 +77,25 @@ if(d3.selectAll('#map').size()>0){
         d3.select('#splash').remove();
       });
 
+
+    var style_source = 'style.json';
+
+    var version = detectIE();
+
+    if (version === false) {
+      //no ie / edge
+    } else if (version >= 12) {
+      style_source = 'tile_style.json';
+    } else {
+      style_source = 'tile_style.json';
+    }
+
     map = new mapboxgl.Map({
       container: 'map',
-      style: 'tile_style.json', //style.json
+      style: 'style.json',
       center: [13.4244,52.5047],
       zoom: 10
     });
-
-    map.on('sourcedata', function(event){
-      console.log('sourcedata', event);
-    });
-
-    map.on('error', function(event){
-      console.log('error', event);
-      if(event.sourceId == 'openmaptiles'){
-        map.setStyle('tile_style.json');
-      }
-    });
-
-    /*
-
-    */
 
     map.addControl(new mapboxgl.NavigationControl(),'bottom-left');
 
@@ -133,6 +131,50 @@ if(d3.selectAll('#map').size()>0){
     .on('end', function(){
       d3.select('#splash').remove();
     });
+}
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  // Test values; Uncomment to check result …
+
+  // IE 10
+  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+  
+  // IE 11
+  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+  
+  // Edge 12 (Spartan)
+  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+  
+  // Edge 13
+  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
 }
 
 function openDetails(id, zoom){
