8ebcc8d643e413ab4a6da949e7bc817a684d9aeb
sebastian-meier <contact@sebastianmeier.eu>
2019-08-06 10:02:46 +0200

    add prediction values

--- data-server/import_predictions.js
+++ data-server/import_predictions.js
@@ -11,16 +11,21 @@ module.exports = {
 					let params = [
 						c.id,
 						c.Datum,
-						c.Vorhersage
+						c.Vorhersage,
+						("p025" in c) ? c.p025 : -1,
+						("p975" in c) ? c.p975 : -1,
+						("p500" in c) ? c.p500 : -1
 					]
 
 					//check if the item already exists
 					let rows = db.prepare("SELECT id FROM predictions WHERE badestellen_id = ? AND date = ?").all([c.id, c.Datum])
 					if(rows.length>0){
+						
 						console.log('already exists', params)
+
 					}else{
 
-						let insert_str = 'INSERT INTO predictions (badestellen_id, date, prediction) VALUES (?,?,?)'
+						let insert_str = 'INSERT INTO predictions (badestellen_id, date, prediction, p025, p975, p500) VALUES (?,?,?,?,?,?)'
 						db.prepare(insert_str).run(params)
 
 					}
