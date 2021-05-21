359386a7a47f9061fd4ba1196127562da608c2ad
Sebastian Meier <contact@sebastianmeier.eu>
2021-05-21 18:40:26 +0200

    impressum & datenschutz

--- src/views/pages/impressum.svelte
+++ src/views/pages/impressum.svelte
@@ -4,54 +4,125 @@
 </script>
 
 <h2>Impressum</h2>
-<h4>Angaben zur Technologiestiftung Berlin</h4>
-<p><b>Technologiestiftung Berlin </b></p>
-<p>
-  Grunewaldstraße 61-62<br /> 10825 Berlin<br /> Telefon +49 30 209 69 99 0<br /> E-Mail: info
-  (at) technologiestiftung-berlin.de
+
+<h3>Herausgeber</h3>
+<p class="lageso">Landesamt für Gesundheit und Soziales Berlin (LAGeSo)<br>Turmstr. 21, Haus A<br>10559 Berlin</p>
+<p class="lageso">Telefon: (030) 90229-0<br>Telefax: (030) 90229-1099</p>
+<p class="lageso"><a href="mailto:poststelle@lageso.berlin.de" title="E-Mail an die Poststelle des LAGeSo">E-Mail</a><br>(nicht für Dokumente mit elektronischer Signatur)</p>
+<p class="lageso">
+  Postanschrift<br>
+  Postfach 31 09 29<br>
+  10639 Berlin
 </p>
-<p>Stiftungsverzeichnis Berlin: AZ 3416/464 B3<br /> USt-Id-Nr.: DE 187 459 128</p>
-<p>
-  Die Technologiestiftung Berlin ist eine rechtsfähige und gemeinnützige Stiftung
-  bürgerlichen Rechts. Sie unterliegt nach dem Berliner Stiftungsgesetz in der Fassung vom
-  11. Dezember 1997 (GVBl. S. 674) der Staatsaufsicht, die von der Senatsverwaltung für
-  Justiz, Salzburger Str. 21-25, 10825 Berlin, geführt wird.
+<h4>
+  Vertretungsberechtigt
+</h4>
+<p class="lageso">
+  Präsident: N.N<br>
+  Ständiger Vertreter: Michael Thiel
 </p>
-<h4>Vertretungsberechtigter Vorstand</h4>
-<p>Nicolas Zimmer (Vorsitz), Steffen Döring, Prof. Dr.-Ing. Ina Schieferdecker</p>
-<h4>Inhaltlich Verantwortliche gemäß §5 TMG</h4>
-<p>Frauke Nippel (Technologiestiftung Berlin)</p>
-<h4>Haftungsausschluss</h4>
-<p>
-  Es wird von uns keine Garantie dafür übernommen, dass die auf dieser Website
-  bereitgestellten Informationen vollständig, richtig und in jedem Fall aktuell sind.
+<h4>
+  Inhaltliche Verantwortung
+</h4>
+<p class="lageso">
+  Silvia Kostner – Z Press -<br>
+  <a href="mailto:presse@lageso.berlin.de" title="E-Mail an die Pressestelle des LAGeSo">E-Mail</a><br>
+  (nicht für Dokmente mit elektronischer Signatur)
 </p>
-<p>
-  Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte
-  externer Links. Wir haben keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung
-  der verlinkten Seiten. Wir distanzieren uns hiermit ausdrücklich von allen inhaltlichen
-  Änderungen, die nach der Linksetzung auf den verlinkten Seiten vorgenommen werden. Für
-  den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+<h4>
+  Social Media
+</h4>
+<p class="lageso">Benjamin Ciupek – Z Press 1 – <a href="mailto:socialmedia@lageso.berlin.de" title="E-Mail an Social Media">E-Mail</a> (nicht für Dokmente mit elektronischer Signatur)</p>
+<ul class="lageso">
+  <li>
+    <a href="https://www.berlin.de/lageso/ueber-uns/artikel.714512.php" title="Weitere Informationen">Weitere Informationen</a>
+  </li>
+</ul>
+<h4>
+  Webmaster
+</h4>
+<p  class="lageso">
+  Sylvia Huesmann-Hendrich – ZS B 31 -<br>
+  Thorsten Busse – ZS B 32 -<br>
+  <a href="mailto:webmaster@lageso.berlin.de" title="E-Mail an die Webmaster des LAGeSo">E-Mail</a><br>
+  (nicht für Dokumente mit elektronischer Signatur)
 </p>
-<p>
-  Wir behalten uns das Recht vor, ohne vorherige Ankündigung Änderungen oder Ergänzungen
-  der bereitgestellten Informationen vorzunehmen.
+<h3>
+  Zentrales E-Mail-Postfach für Dokumente mit elektronischer Signatur
+</h3>
+<p  class="lageso">
+  Das Landesamt für Gesundheit und Soziales verfügt über ein zentrales E-Mail-Postfach, an das auch Dokumente verschickt werden können, die eine <a href="https://www.berlin.de/sen/inneres/moderne-verwaltung/e-government/vertrauensdienste/signatur/artikel.232354.php" title="Weitere Informationen zur qualifizierten elektronischen Signatur">qualifizierte elektronische Signatur</a> haben. Grundlage hierfür ist § 3 a Abs. 1 Verwaltungsverfahrensgesetz.
 </p>
-<p>
-  Wir sind bestrebt, in allen Publikationen geltende Urheberrechte zu beachten. Sollte es
-  trotzdem zu einer Urheberrechtsverletzung kommen, werden wir das entsprechende Objekt
-  nach Benachrichtigung aus seiner Publikation entfernen bzw. mit dem Urheberrecht
-  kenntlich machen.
+<p  class="lageso">
+  E-Mail: <a href="mailto:post@lageso.berlin.de" title="Link zum Postfach: post@lageso.berlin.de">post@lageso.berlin.de</a><br>
+  Eine wirksame Übermittlung verschlüsselter Dateien ist gegenwärtig ausgeschlossen.
 </p>
-<p>
-  Alle auf den Webseiten genannten und gegebenenfalls durch Dritte geschützte Marken- und
-  Warenzeichen unterliegen uneingeschränkt den Bestimmungen des jeweils gültigen
-  Kennzeichenrechts und den Besitzrechten der jeweiligen eingetragenen Eigentümer.
+<h4>
+  DE Mail
+</h4>
+<p  class="lageso">
+  post@lageso-berlin.de-mail.de
 </p>
-<p>
-  Dieser Haftungshinweis ist als Teil des Internetangebotes der Technologiestiftung Berlin
-  und des Fördervereins Technologiestiftung Berlin e. V. zu betrachten. Sofern Teile oder
-  einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr oder
-  nicht vollständig entsprechen sollten, bleiben die übrigen Teile des Dokuments in ihrem
-  Inhalt und ihrer Gültigkeit hiervon unberührt.
+<h3>
+  Benutzerhinweise und Barrierefreiheit
+</h3>
+<p  class="lageso">
+  <a href="https://www.berlin.de/rbmskzl/service/stadtinformationssystem-berlin-de/artikel.123149.php" title="Link zu den Hinweisen für Berlin.de-Nutzer zur Bedienung der Seiten und zur Barrierefreiheit">Hinweise für Berlin.de-Nutzer zur Bedienung der Seiten zur Barrierefreiheit</a>
+</p>
+<h3>
+  Technischer Betrieb
+</h3>
+<p  class="lageso">
+  <a href="http://www.technologiestiftung-berlin.de/" class=" extern" target="_blank" title="Link zum Auftritt der gemeinnützigen Technologiestiftung Berlin (Öffnet in neuem Fenster)">Technologiestiftung Berlin</a><br>
+  Grunewaldstraße 61-62<br />
+  10825 Berlin<br />
+  Tel.: +49 30 209 69 99-0<br />
+  E-Mail: <a href="info@technologiestiftung-berlin.de" title="Link zum Postfach:info@technologiestiftung-berlin">info@technologiestiftung-berlin</a>
+</p>
+<h3>
+  ethikPool – Antragsportal der Ethik-Kommission des Landes Berlin
+</h3>
+<h4>
+  Inhaltlich Verantwortlich
+</h4>
+<p  class="lageso">
+  Abt. IV E, Antje Kettner-Ottilie<br>
+  Telefon: (030) 90229-1220<br>
+  Telefax: (030) 9028-3383<br>
+  e-mail: antje.kettner-ottilie@lageso.berlin.de<br>
+  (nicht für Dokumente mit elektronischer Signatur)
+</p>
+<h5>
+  Technischer Betrieb
+</h5>
+<p  class="lageso">
+  ethikPool ist ein Produkt der smart-Q Softwaresysteme GmbH<br>
+  BioMedizinZentrum Bochum<br>
+  Universitätsstr. 136<br>
+  44799 Bochum
+</p>
+<p  class="lageso">
+  Tel.: 0234 / 60 600 760<br>
+  Fax: 0234 / 60 600 762
+</p>
+<p  class="lageso">
+  E-Mail:<a href="mailto:info@smart-q.de" title="E-Mail an Smart q">info@smart-q.de</a>
+</p>
+<p  class="lageso">
+  weitere Informationen: <a href="https://www.smart-q.de" class=" extern" target="_blank" title="Internet smart q (Öffnet in neuem Fenster)">https://www.smart-q.de</a>
+</p>
+<h4>
+  Haftungshinweis
+</h4>
+<p  class="lageso">
+  Alle Angaben erfolgen ohne Gewähr. Eine Haftung für Schäden, die sich aus der Verwendung der abgerufenen Informationen und Dienste ergeben, durch das Land Berlin beziehungsweise die einstellenden Institutionen ist ausgeschlossen.
+</p>
+<p  class="lageso">
+  Bei “Links” handelt es sich allerdings stets um “lebende” (dynamische) Verweisungen. Das Landesamt für Gesundheit und Soziales Berlin hat bei der erstmaligen Verknüpfung zwar den fremden Inhalt daraufhin überprüft, ob durch ihn eine mögliche zivilrechtliche oder strafrechtliche Verantwortlichkeit ausgelöst wird. Es ist aber nicht dazu verpflichtet, die Inhalte, auf die es in seinem Angebot verweist, ständig auf Veränderungen zu überprüfen, die eine Verantwortlichkeit neu begründen könnten.
+</p>
+<h4>
+  Urheberrecht
+</h4>
+<p  class="lageso">
+  Layout und Gestaltung des Angebotes insgesamt sowie einzelne Elemente sind urheberrechtlich geschützt. Gleiches gilt auch für die inhaltlichen Beiträge. Veränderungen daran dürfen nicht vorgenommen werden, Weiterverwendung und Vervielfältigung sind nur zu privaten Zwecken gestattet. Eine öffentliche Verwendung des Angebots darf nur mit Zustimmung des Landesamtes für Gesundheit und Soziales erfolgen.
 </p>
