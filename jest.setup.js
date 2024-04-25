76b3437c6d1d961884b595f055821d0f0cf20e8d
ff6347 <hash@ff6347.email>
2024-04-25 09:57:14 +0200

    test(swimapi): Update test to mock swimapi

--- jest.setup.js
+++ jest.setup.js
@@ -1,30 +1,29 @@
-const fetchMock = require('fetch-mock');
-fetchMock.config.sendAsJson = false;
+const fetchMock = require('fetch-mock')
+fetchMock.config.sendAsJson = false
 
 const fs = require('fs')
 
 global.__global = {
   env: {
-
-    URL: "http://localhost:5000",
-    isProd: false,
+    URL: 'http://localhost:5000',
+    isProd: false
     // ...require('dotenv').config().parsed
   }
 }
 
 fetchMock.get(
-  'https://flsshygn-lageso-berlin-prediction-merge-dev.s3.eu-central-1.amazonaws.com/app/data.csv',
-  fs.readFileSync(__dirname + '/src/__tests__/data/data.csv', 'utf8')
-  )
+  'http://localhost:5000/.netlify/functions/swimapi',
+  fs.readFileSync(__dirname + '/src/__tests__/data/data.json', 'utf8')
+)
 
 fetchMock.get(
   'http://localhost:5000/assets/data/new_build.csv',
   fs.readFileSync(__dirname + '/src/__tests__/data/new_build.csv', 'utf8')
-  )
+)
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => {
   return {
-    'default': {
+    default: {
       accessToken: '',
       GeolocateControl: jest.fn(),
       Map: jest.fn(() => ({
@@ -32,13 +31,13 @@ jest.mock('mapbox-gl/dist/mapbox-gl', () => {
         on: jest.fn(),
         remove: jest.fn(),
         fitBounds: jest.fn(),
-        flyTo: jest.fn(),
+        flyTo: jest.fn()
       })),
       NavigationControl: jest.fn(),
       Marker: jest.fn(() => ({
         setLngLat: jest.fn(),
-        addTo: jest.fn(),
-      })),
+        addTo: jest.fn()
+      }))
     }
   }
-});
+})
